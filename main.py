12946a1e2345cc691285cda21d22a971770d9f69
justindorsch <jdorsch.social@gmail.com>
2026-06-23 12:22:22 -0400

    Added asteroid destruction

--- main.py
+++ main.py
@@ -32,11 +32,16 @@ def main():
                 return
         for obj in updatable:
             obj.update(dt)
-        for obj in asteroids:
-            if obj.collides_with(player):
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
